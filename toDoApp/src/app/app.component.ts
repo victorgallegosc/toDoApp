a2d2c2e56b8a349b79419838c8b295757deb67f5
Victor Gallegos <victor.gallegos@epicor.com>
2024-02-26 13:52:45 -0600

    Added item file and the getter to return items depending on the selected filter

--- toDoApp/src/app/app.component.ts
+++ toDoApp/src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'toDoApp';
+
+  filter: "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false }
+  ];
+
+  get items() {
+    if (this.filter === "all") {
+      return this.allItems;
+    }
+    return this.allItems.filter((item) =>
+      this.filter === "done" ? item.done : !item.done
+    )
+  }
+
 }
